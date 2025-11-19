c80e2390e6374a68e94a5156d10fed7b4e1d0dd7
Youa2024 <youaldb@gmail.com>
2025-11-19 17:10:48 +0700

    update

--- pages/Price/Price.vue
+++ pages/Price/Price.vue
@@ -159,12 +159,33 @@
             <v-row justify="start" align="start"
               ><v-col cols="12" md="6" sm="6">
                 <v-btn
+                  color="grey"
+                  class="mr-2"
+                  rounded="xl"
+                  variant="outlined"
+                  @click="cloarData()"
+                  ><v-icon class="mr-4">mdi-cancel</v-icon
+                  >{{ $t("btn_cancel") }}</v-btn
+                >
+                <v-btn
+                  v-if="edit == false"
                   color="primary"
                   rounded="xl"
                   variant="outlined"
                   type="submit"
+                  @click="insertPrict()"
                   ><v-icon class="mr-4">mdi-content-save-all</v-icon
                   >{{ $t("save") }}</v-btn
+                >
+                <v-btn
+                  v-else
+                  color="purple"
+                  rounded="xl"
+                  variant="outlined"
+                  type="submit"
+                  @click="updatePrice()"
+                  ><v-icon class="mr-4">mdi-content-save-all</v-icon
+                  >{{ $t("btn_edit") }}</v-btn
                 ></v-col
               ></v-row
             >
@@ -176,17 +197,32 @@
         <v-card-text>
           <v-data-table
             :headers="headers"
-            :items="companies_list"
+            :items="allData"
+            :search="search"
             hide-actions
             class="elevation-1"
             pagination.sync="pagination"
             item-key="id"
-            search="search"
           >
+            <template #item.id="{ index, item }">
+              {{ index + 1 }}
+            </template>
+            <template #item.actions="{ item }">
+              <div class="d-flex">
+                <v-btn
+                  color="blue"
+                  rounded="xl"
+                  variant="outlined"
+                  @click="showEdit(item)"
+                  ><v-icon>mdi-pen</v-icon>{{ $t("btn_edit") }}</v-btn
+                >
+              </div>
+            </template>
           </v-data-table>
         </v-card-text>
       </v-card>
     </v-card>
+    <MLoading v-model="loading"></MLoading>
   </v-container>
 </template>
 
@@ -204,7 +240,12 @@ const thb_unit = ref(null);
 const thb_package = ref(null);
 const usd_unit = ref(null);
 const usd_package = ref(null);
-const userName = ref(null);
+const search = ref(null);
+const allData = ref([]);
+const key_id = ref(null);
+const edit = ref(false);
+const loading = ref(false);
+const { showSuccess, showWarning, showError } = useAlert();
 // role for feild
 const rules = [
   (value) => {
@@ -213,8 +254,7 @@ const rules = [
   },
 ];
 // format as user types
-const price_package = ref("");
-const price_unit = ref("");
+
 const formatCurrency = () => {
   let value = currencyInput.value.replace(/[^\d.]/g, ""); // allow only numbers & dot
   if (value) {
@@ -227,39 +267,117 @@ const formatCurrency = () => {
   }
 };
 // table header
-const headers_product = ref([
-  { title: "#", key: "1", align: "start" },
-  { title: t("product_name"), key: "grounId", align: "start" },
-  { title: t("product_type"), key: "comName", align: "end" },
-  { title: t("product_type"), key: "comName", align: "end" },
-]);
-const headers = ref([
-  { title: "#", key: "1", align: "start" },
-  { title: t("product_name"), key: "grounId", align: "start" },
 
-  { title: t("product_type"), key: "comName", align: "end" },
-  { title: t("price_unit"), key: "comType", align: "start" },
-  { title: t("price_package"), key: "province", align: "start" },
-  { title: t("conpany_name"), key: "grounId", align: "start" },
-  { title: t("branch_name"), key: "grounId", align: "start" },
-]);
-const companies_list = [
+const headers = ref([
+  { title: "#", key: "id", align: "start" },
+  { title: t("product_name"), key: "productName", align: "start" },
+  { title: t("price_unit") + " " + "LAK", key: "lakUnit", align: "start" },
   {
-    groupId: "Ford Mustang",
-    comName: 450,
-    comType: "Gasoline",
-    province: "USA",
-    district: 55000,
-    village: "Gasoline",
-    phone: "USA",
-    branchAtm: 55000,
+    title: t("price_package") + " " + "LAK",
+    key: "lakPackage",
+    align: "start",
   },
-];
+  { title: t("price_unit") + " " + "THB", key: "thbUnit", align: "start" },
+  {
+    title: t("price_package") + " " + "THB",
+    key: "thbPackage",
+    align: "start",
+  },
+  {
+    title: t("price_package") + " " + "USD",
+    key: "usdPackage",
+    align: "start",
+  },
+  { title: t("price_unit") + " " + "USD", key: "usdUnit", align: "start" },
+  { title: t("branch_name"), key: "branchName", align: "start" },
+  { title: t("crate_date"), key: "createDate", align: "start" },
+  { title: t("actions"), key: "actions", align: "start" },
+]);
+
 // Method========================
 onMounted(() => {
+  getAllPrices();
   getProducts();
   getBranches();
 });
+const cloarData = () => {
+  edit.value = false;
+  key_id.value = null;
+  branch.value = null;
+  product.value = null;
+  lak_unit.value = null;
+  lak_package.value = null;
+  thb_unit.value = null;
+  thb_package.value = null;
+  usd_unit.value = null;
+  usd_package.value = null;
+};
+const updatePrice = async () => {
+  loading.value = true;
+  var body = {
+    priceId: key_id.value,
+    branchId: branch.value,
+    productId: product.value,
+    lakUnit: lak_unit.value,
+    lakPackage: lak_package.value,
+    thbUnit: thb_unit.value,
+    thbPackage: thb_package.value,
+    usdUnit: usd_unit.value,
+    usdPackage: usd_package.value,
+    updateBy: localStorage.getItem("user"),
+  };
+  const res = await mainApi.put("updatePrice", body);
+  if (res.data.status == "00") {
+    loading.value = false;
+    cloarData();
+    getAllPrices();
+    showSuccess(res.data.message);
+  } else {
+    showError(res.data.message);
+  }
+};
+const showEdit = (item) => {
+  edit.value = true;
+  key_id.value = item.priceId;
+  branch.value = item.branchId;
+  product.value = item.productId;
+  lak_unit.value = item.lakUnit;
+  lak_package.value = item.lakPackage;
+  thb_unit.value = item.thbUnit;
+  thb_package.value = item.thbPackage;
+  usd_unit.value = item.usdUnit;
+  usd_package.value = item.usdPackage;
+};
+const getAllPrices = async () => {
+  const res = await mainApi.post("getPrices");
+  if (res.data.status == "00") {
+    allData.value = res.data.dataRes;
+    console.log("allData=============", allData.value);
+  } else {
+    showError(res.data.message);
+  }
+};
+const insertPrict = async () => {
+  var body = {
+    branchId: branch.value,
+    productId: product.value,
+    lakUnit: lak_unit.value,
+    lakPackage: lak_package.value,
+    thbUnit: thb_unit.value,
+    thbPackage: thb_package.value,
+    usdUnit: usd_unit.value,
+    usdPackage: usd_package.value,
+    createBy: localStorage.getItem("user"),
+  };
+  const res = await mainApi.post("insertPrice", body);
+  if (res.data.status == "00") {
+    loading.value = false;
+    cloarData();
+    getAllPrices();
+  } else {
+    showError(res.data.message);
+  }
+};
 const getProducts = async () => {
   const res = await mainApi.get("getProducts");
   if (res.data.status == "00") {
